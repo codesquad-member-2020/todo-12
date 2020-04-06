e688e04a9a3a4e5830a5613f0639b9b734a4df7f
1Consumption <hanseop95@gmail.com>
2020-04-06 16:17:10 +0900

    [Feat] container view 적용
    tableView를 자식으로 가짐

--- iOS/ToDo/ToDo/ViewController.swift
+++ iOS/ToDo/ToDo/ViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var todoViewController: TableViewController?
+    private var inProgressViewController: TableViewController?
+    private var doneViewController: TableViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "todo" {
+            todoViewController = segue.destination as? TableViewController
+            todoViewController?.tableView.backgroundColor = .blue
+        }
+        
+        if segue.identifier == "inProgress" {
+            inProgressViewController = segue.destination as? TableViewController
+            inProgressViewController?.tableView.backgroundColor = .white
+        }
+        
+        if segue.identifier == "done" {
+            doneViewController = segue.destination as? TableViewController
+            doneViewController?.tableView.backgroundColor = .red
+        }
     }
 }
 
