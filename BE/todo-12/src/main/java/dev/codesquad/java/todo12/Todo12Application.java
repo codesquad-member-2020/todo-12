0eb5acf97dc7e63870f94610b311dba2f0d7c316
wooody92 <pkower4@naver.com>
2020-04-06 17:18:26 +0900

    [feat] 개발환경 설정 #3
    - 개발환경 설정
    - git ignore 추가

---
+++ BE/todo-12/src/main/java/dev/codesquad/java/todo12/Todo12Application.java
@@ -0,0 +1,13 @@
+package dev.codesquad.java.todo12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todo12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Todo12Application.class, args);
+	}
+
+}
