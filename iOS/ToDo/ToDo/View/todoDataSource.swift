d6a2eb4ba02ddc1b1ccef9a4017f1a9654a630f3
1Consumption <hanseop95@gmail.com>
2020-04-07 19:34:24 +0900

    [feat] cell force touch(or 3d touch)시 context menu가 팝업 되도록 기능 추가 #8

--- iOS/ToDo/ToDo/View/todoDataSource.swift
+++ iOS/ToDo/ToDo/View/todoDataSource.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class todoDataSource: NSObject, UITableViewDataSource {
+    
+    let contextDelegate = contextMenuDelegate()
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         20
     }
@@ -16,6 +19,8 @@ class todoDataSource: NSObject, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell") as? taskCell else {return UITableViewCell()}
         
+        let interaction = UIContextMenuInteraction(delegate: contextDelegate)
+        cell.addInteraction(interaction)
         return cell
     }
 }
