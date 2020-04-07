8f36dfe3c63012845cfa56fd7ee0f8b8c875eb1a
1Consumption <hanseop95@gmail.com>
2020-04-07 22:11:06 +0900

    [feat] 스와이프 하여 삭제 기능 추가 #8
    임시 데이터를 model로 넣어놨으며, 이를 바탕으로 삭제 기능 추가

--- iOS/ToDo/ToDo/View/TaskViewController.swift
+++ iOS/ToDo/ToDo/View/TaskViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class TaskViewController: UIViewController, UITableViewDelegate {
-
+    
     @IBOutlet weak var totalTaskLabel: UILabel!
     @IBOutlet weak var taskTabelView: UITableView!
     
@@ -19,6 +19,7 @@ class TaskViewController: UIViewController, UITableViewDelegate {
         super.viewDidLoad()
         setLabelRadius()
         taskTabelView.dataSource = dataSource
+        taskTabelView.delegate = self
         totalTaskLabel.text = String(taskTabelView.numberOfRows(inSection: 0))
     }
     
@@ -26,5 +27,15 @@ class TaskViewController: UIViewController, UITableViewDelegate {
         let superViewHeight = totalTaskLabel.superview?.layer.frame.height ?? 0
         totalTaskLabel.clipsToBounds = true
         totalTaskLabel.layer.cornerRadius = superViewHeight * 0.24
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title:  "삭제", handler: { action, view, completionHandler in
+            self.dataSource.model.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            completionHandler(true)
+        })
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction])
     }
 }
--- iOS/ToDo/ToDo/View/todoDataSource.swift
+++ iOS/ToDo/ToDo/View/todoDataSource.swift
@@ -11,14 +11,15 @@ import UIKit
 class todoDataSource: NSObject, UITableViewDataSource {
     
     let contextDelegate = contextMenuDelegate()
-    
+    var model = [1,2,3,4,5,6,7,8,9,10]
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        20
+        return model.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell") as? taskCell else {return UITableViewCell()}
         
+        cell.titleLabel.text = String(model[indexPath.row])
         let interaction = UIContextMenuInteraction(delegate: contextDelegate)
         cell.addInteraction(interaction)
         return cell
