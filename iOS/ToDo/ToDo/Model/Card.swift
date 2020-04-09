ba82e83bf3d51fa987dfe0c5de7a54c46527dab2
1Consumption <hanseop95@gmail.com>
2020-04-09 16:30:15 +0900

    [feat] model 클래스 생성 #16
    서버에서 받은 데이터를 decode 해서 저장할 클래스

---
+++ iOS/ToDo/ToDo/Model/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  ToDo
+//
+//  Created by 신한섭 on 2020/04/09.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+struct Card: Codable {
+    var id: Int
+    var title: String?
+    var content: String
+    var author: String
+}
---
+++ iOS/ToDo/ToDo/Model/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  ToDo
+//
+//  Created by 신한섭 on 2020/04/09.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import Foundation
+
+struct Model: Codable {
+    var categories: [Category]
+}
