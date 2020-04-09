6a665f620acd45d6bd738b10f7380b289bb98349
1Consumption <hanseop95@gmail.com>
2020-04-09 20:47:12 +0900

    [refactor] CardManager 클래스 삭제 #16
    struct는 값 타입이라서 참조를 할 수 없음. 따라서 CardManager의 역할을 수행하지 못함 -> 새로운 인스턴스를 만들기 때문
    CardManager의 역할을 Category 객체에서 하도록 변경

--- iOS/ToDo/ToDo/Model/Category.swift
+++ iOS/ToDo/ToDo/Model/Category.swift
@@ -12,4 +12,27 @@ struct Category: Codable {
     var id: Int
     var name: String
     var cards: [Card]
+    var count: Int {
+        return cards.count
+    }
+    
+    func title(of index: Int) -> String {
+        return cards[index].title ?? "제목 없음"
+    }
+    
+    func content(of index: Int) -> String {
+        return cards[index].content
+    }
+    
+    func author(of index: Int) -> String {
+        return cards[index].author
+    }
+    
+    mutating func append(card: Card) {
+        cards.append(card)
+    }
+    
+    mutating func remove(at index: Int) {
+        cards.remove(at: index)
+    }
 }
