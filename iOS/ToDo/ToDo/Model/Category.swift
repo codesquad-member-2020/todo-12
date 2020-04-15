1ac61e87be8d738a0fa30390e6673700161c5485
1Consumption <hanseop95@gmail.com>
2020-04-15 23:35:31 +0900

    [refactor] 사용하지 않는 method 제거 #42

--- iOS/ToDo/ToDo/Model/Category.swift
+++ iOS/ToDo/ToDo/Model/Category.swift
@@ -11,42 +11,6 @@ import Foundation
 struct Category: Codable {
     var id: Int
     var name: String
-    var cards: [Card] {
-        didSet {
-            NotificationCenter.default.post(name: .cardChanged,
-                                            object: self)
-        }
-    }
-    var count: Int {
-        return cards.count
-    }
-    
-    func title(of index: Int) -> String {
-        return cards[index].title ?? "제목 없음"
-    }
-    
-    func content(of index: Int) -> String {
-        return cards[index].content
-    }
-    
-    func author(of index: Int) -> String {
-        return "author by " + (cards[index].author ?? "")
-    }
-    
-    func card(at index: Int) -> Card {
-        return cards[index]
-    }
-    
-    mutating func append(card: Card) {
-        cards.append(card)
-    }
-    
-    mutating func insert(_ newElement: Card, at index: Int) {
-        cards.insert(newElement, at: index)
-    }
-    
-    mutating func remove(at index: Int) {
-        cards.remove(at: index)
-    }
+    var cards: [Card]
 }
 
