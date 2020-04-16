2707c193a160876b473e4401d930ced6a07ac869
1Consumption <hanseop95@gmail.com>
2020-04-17 02:37:21 +0900

    [feat] action에 따라 다른 text를 보여주는 메소드 작성  #61

--- iOS/ToDo/ToDo/View/HistoryView/HistoryTableViewController.swift
+++ iOS/ToDo/ToDo/View/HistoryView/HistoryTableViewController.swift
@@ -28,12 +28,40 @@ class HistoryTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "historyCell") as? HistoryCell else {return UITableViewCell()}
-        guard let id = historys?[indexPath.row].id else {return UITableViewCell()}
-        cell.contentLabel.text = String(id)
+        guard let history = historys?[indexPath.row] else {return UITableViewCell()}
+        DispatchQueue.main.async {
+            switch history.action {
+            case "moved":
+                cell.contentLabel.text = self.moved(history: history)
+                break
+            case "added":
+                cell.contentLabel.text = self.added(history: history)
+                break
+            case "deleted":
+                cell.contentLabel.text = self.deleted(history: history)
+            default:
+                break
+            }
+        }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return historys?.count ?? 0
+    }
+    
+    func moved(history: History) -> String {
+        let moved = "@\(history.userId) \(history.action) \(history.cardTitle ?? "제목 없음") from \(history.fromCategory ?? "") to \(history.toCategory)"
+        return moved
+    }
+    
+    func added(history: History) -> String {
+        let added = "@\(history.userId) \(history.action) \(history.cardTitle ?? "제목 없음") to \(history.toCategory)"
+        return added
+    }
+    
+    func deleted(history: History) -> String {
+        let deleted = "@\(history.userId) \(history.action) \(history.cardTitle ?? "제목 없음") fromo \(history.fromCategory ?? "")"
+        return deleted
     }
 }
