79fbd85b37812944901592d105e6608760251bc6
1Consumption <hanseop95@gmail.com>
2020-04-17 02:04:48 +0900

    [feat] 커스텀 셀 연결 #61

---
+++ iOS/ToDo/ToDo/View/HistoryView/Cell/HistoryCell.swift
@@ -0,0 +1,16 @@
+//
+//  HistoryCell.swift
+//  ToDo
+//
+//  Created by 신한섭 on 2020/04/17.
+//  Copyright © 2020 신한섭. All rights reserved.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageVIew: UIImageView!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+}
