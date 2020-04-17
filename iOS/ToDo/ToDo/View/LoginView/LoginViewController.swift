32538ff4d281c4f5644950e14790c9287d01e13e
1Consumption <hanseop95@gmail.com>
2020-04-17 16:14:33 +0900

    [feat] 로그인 실패 이유 사용자에게 보여줌

--- iOS/ToDo/ToDo/View/LoginView/LoginViewController.swift
+++ iOS/ToDo/ToDo/View/LoginView/LoginViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
     @IBOutlet weak var idLabel: UITextField!
     @IBOutlet weak var passwordLabel: UITextField!
+    @IBOutlet weak var wrongIDLabel: UILabel!
+    @IBOutlet weak var wrongPasswordLabel: UILabel!
     @IBAction func login(_ sender: UIButton) {
         guard let id = idLabel.text else {return}
         guard let password = passwordLabel.text else {return}
@@ -20,7 +24,27 @@ class LoginViewController: UIViewController {
         do {
             let body = try encoder.encode(loginInfo)
             NetworkConnection.requestToken(body: body, failureHandler: {
-                print($0.statusCode)
+                if $0.statusCode == 404 {
+                    DispatchQueue.main.async {
+                        self.wrongIDLabel.alpha = 1
+                        self.wrongIDLabel.textColor = .red
+                        self.wrongIDLabel.text = "잘못된 ID 입니다."
+                        self.wrongPasswordLabel.alpha = 0
+                        self.idLabel.layer.borderColor = UIColor.red.cgColor
+                        self.idLabel.layer.borderWidth = 1
+                        self.passwordLabel.layer.borderWidth = 0
+                    }
+                } else if $0.statusCode == 401 {
+                    DispatchQueue.main.async {
+                        self.wrongPasswordLabel.alpha = 1
+                        self.wrongPasswordLabel.textColor = .red
+                        self.wrongPasswordLabel.text = "잘못된 패스워드 입니다."
+                        self.wrongIDLabel.alpha = 0
+                        self.passwordLabel.layer.borderColor = UIColor.red.cgColor
+                        self.passwordLabel.layer.borderWidth = 1
+                        self.idLabel.layer.borderWidth = 0
+                    }
+                }
             }) {
                 NotificationCenter.default.post(name: .postLoginSuccess,
                                                 object: nil)
@@ -41,8 +65,16 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupLabelRadius()
     }
+    
+    private func setupLabelRadius() {
+        loginBtn.clipsToBounds = true
+        cancelBtn.clipsToBounds = true
+        loginBtn.layer.cornerRadius = 10
+        cancelBtn.layer.cornerRadius = 10
+    }
+    
 }
 
 extension Notification.Name {
