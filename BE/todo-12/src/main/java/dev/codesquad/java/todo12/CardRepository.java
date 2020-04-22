9942e0aa8ec43d67189b6cb01715f27a74d92dc6
wooody92 <pkower4@naver.com>
2020-04-22 22:54:42 +0900

    [refactor] 불필요 주석 제거
    - Repository : 불필요한 주석 제거 했습니다.

--- BE/todo-12/src/main/java/dev/codesquad/java/todo12/CardRepository.java
+++ BE/todo-12/src/main/java/dev/codesquad/java/todo12/CardRepository.java
@@ -7,12 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface CardRepository extends CrudRepository<Card, Long> {
-//    @Query("Select * from card where card.deleted = false and card.category = :id")
-//    Optional<List<Card>> findCardsByIdDeletedFalse(Long id);
-//
-//    @Query("Select * from card where card.deleted = false and id = :id")
-//    Optional<Card> findByIdDeletedFalse(Long id);
-
     @Query("Select * from card where card.category = :id")
     Optional<List<Card>> findAllById(Long id);
 }
